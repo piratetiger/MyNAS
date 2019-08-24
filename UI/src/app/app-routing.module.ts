5e41d0ed87d388f5572bffdd6841bb80303b1348
piratetiger <charlesli1120@hotmail.com>
2019-08-24 21:52:46 +0800

    update main

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppMainComponent } from './app-main/app-main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppMainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppMainComponent } from './app-main/app-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppMainComponent
   ],
   imports: [
     BrowserModule,
