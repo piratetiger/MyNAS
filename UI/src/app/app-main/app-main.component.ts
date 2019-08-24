afc2b5a35f40439717bc3bcf2da5bf78b7eee0da
piratetiger <charlesli1120@hotmail.com>
2019-08-24 22:25:31 +0800

    update route component

--- UI/src/app/app-main/app-main.component.ts
+++ UI/src/app/app-main/app-main.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './app-main.component.html',
-  styleUrls: ['./app-main.component.scss']
+    selector: 'app-main',
+    templateUrl: './app-main.component.html',
+    styleUrls: ['./app-main.component.scss']
 })
 export class AppMainComponent {
-  title = 'UI';
+    constructor(@Inject(Router) private router: Router) { }
+
+    public updateRoute(path: string) {
+        this.router.navigateByUrl('/' + path);
+    }
 }
