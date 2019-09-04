4335ad09ebf83d73b7ce572abb9393e5813cbde5
piratetiger <tigerwing_tc@hotmail.com>
2019-09-04 22:39:50 +0800

    update 401 redirect

--- frontend/src/app/http-interceptor/auth-interceptor.ts
+++ frontend/src/app/http-interceptor/auth-interceptor.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import {
-    HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
+    HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse
 } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import * as store from 'store';
 import { LoginModel } from '../app.models/login-model';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
+    constructor(private router: Router) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const loginInfo: LoginModel = store.get('loginInfo');
@@ -18,6 +22,14 @@ export class AuthInterceptor implements HttpInterceptor {
                 .set('x-login-user', loginInfo.username);
         }
 
-        return next.handle(req.clone({ headers: headers }));
+        return next.handle(req.clone({ headers: headers })).pipe(
+            catchError(err => {
+                if (err instanceof HttpErrorResponse) {
+                    if (err.status === 401) {
+                        this.router.navigate(['/login']);
+                    }
+                }
+                return throwError(err);
+            }));
     }
 }
