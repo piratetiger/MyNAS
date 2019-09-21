e4562585029c00520ee9afdd0e7450e95403b504
piratetiger <tigerwing_tc@hotmail.com>
2019-09-21 17:29:05 +0800

    update image popup

--- frontend/src/app/infrastructure/components/image-lightbox/image-lightbox.component.ts
+++ frontend/src/app/infrastructure/components/image-lightbox/image-lightbox.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewEncapsulation, Input } from '@angular/core';
 import { ImageModel } from '../../models/image-model';
 import { ApiService } from '../../services/api.service/api.service';
+import { DialogService } from 'primeng/api';
+import { ImageViewerComponent } from './image-viewer/image-viewer.component';
 
 @Component({
     selector: 'image-lightbox',
@@ -11,9 +13,23 @@ import { ApiService } from '../../services/api.service/api.service';
 export class ImageLightboxComponent {
     @Input() images: ImageModel[] = [];
 
-    constructor(private service: ApiService) { }
+    constructor(private service: ApiService, private dialogService: DialogService) { }
 
     public getImageUrl(image: string) {
         return `${this.service.serviceUrls.getImage}?thumb=true&name=${image}`;
+    }
+
+    public showDetail(image: ImageModel) {
+        const ref = this.dialogService.open(ImageViewerComponent, {
+            data: {
+                images: this.images,
+                current: image
+            },
+            header: '',
+            width: '70%',
+            height: '70%',
+            styleClass: 'image-lightbox-detail',
+            dismissableMask: true
+        });
     }
 }
