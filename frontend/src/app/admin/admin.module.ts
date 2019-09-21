b18b4e184350111464dd3ec1ccc05e881332c816
piratetiger <tigerwing_tc@hotmail.com>
2019-09-21 22:48:28 +0800

    update admin module

---
+++ frontend/src/app/admin/admin.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+// import { TableModule } from 'primeng/table';
+
+import { AppServiceModule } from '../infrastructure/services/app.service.module';
+
+import { AppUserConfigComponent } from './app-user-config/app-user-config.component';
+
+
+@NgModule({
+    declarations: [
+        AppUserConfigComponent,
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpClientModule,
+
+        AppServiceModule,
+
+        // TableModule
+    ],
+    exports: [
+        AppUserConfigComponent,
+    ],
+    providers: [
+    ],
+})
+export class AdminModule { }
---
+++ frontend/src/app/admin/app-user-config/app-user-config.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-user-config',
+    templateUrl: './app-user-config.component.html',
+    styleUrls: ['./app-user-config.component.scss']
+})
+export class AppUserConfigComponent {
+}
