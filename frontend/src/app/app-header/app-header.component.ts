40c755962aed5bb70e9e7d0c97bdf844ac86fcc9
piratetiger <tigerwing_tc@hotmail.com>
2019-10-01 21:49:39 +0800

    login logout

--- frontend/src/app/app-header/app-header.component.ts
+++ frontend/src/app/app-header/app-header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppService } from '../infrastructure/services/app.service/app.service';
 
 @Component({
     selector: 'app-header',
@@ -6,4 +8,14 @@ import { Component } from '@angular/core';
     styleUrls: ['./app-header.component.scss'],
 })
 export class AppHeaderComponent {
+    public get user() {
+        return this.service.userInfo;
+    }
+
+    constructor(private service: AppService, private router: Router) { }
+
+    public logout() {
+        this.service.userInfo = null;
+        this.router.navigate(['/login']);
+    }
 }
