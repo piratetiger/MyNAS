451d1b00dc0f91568e906c4de6a04d38eecafe12
piratetiger <tigerwing_tc@hotmail.com>
2019-10-03 23:12:15 +0800

    update log folder

--- backend/MyNAS.Site/Filters/AuditLogAttribute.cs
+++ backend/MyNAS.Site/Filters/AuditLogAttribute.cs
@@ -5,11 +5,27 @@ namespace MyNAS.Site
 {
     public class AuditLogAttribute : ActionFilterAttribute
     {
+        private static Logger _logger;
+
+        protected Logger Logger
+        {
+            get
+            {
+                if (_logger == null)
+                {
+                    var factory = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config");
+                    _logger = factory.GetCurrentClassLogger();
+                }
+
+                return _logger;
+            }
+        }
+
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             var args = context.ActionArguments;
-            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
-            logger.Log(LogLevel.Info, args);
+            Logger.Log(LogLevel.Info, args);
+
             base.OnActionExecuting(context);
         }
     }
