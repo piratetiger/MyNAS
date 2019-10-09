e380ebfc24e2ce2fcfd95876020abe4bc624e723
piratetiger <tigerwing_tc@hotmail.com>
2019-10-09 22:52:28 +0800

    increase video upload size

--- backend/MyNAS.Site/Areas/Api/Controllers/VideosController.cs
+++ backend/MyNAS.Site/Areas/Api/Controllers/VideosController.cs
@@ -69,8 +69,8 @@ namespace MyNAS.Site.Areas.Api.Controllers
 
         [HttpPost("add")]
         [Authorize(Policy = "UserBase")]
-        [RequestFormLimits(MultipartBodyLengthLimit = 73400320)]
-        [RequestSizeLimit(73400320)]
+        [RequestFormLimits(MultipartBodyLengthLimit = 209715200)]
+        [RequestSizeLimit(209715200)]
         public object UploadVideo(IEnumerable<IFormFile> files, [FromForm] string date)
         {
             var videoList = new List<VideoModel>();
